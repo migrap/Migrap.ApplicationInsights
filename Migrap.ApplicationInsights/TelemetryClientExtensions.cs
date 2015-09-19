ad299adcb66f0fa7b32951bf1399233bd2e63cd8
Michael Park <migrap@outlook.com>
2015-09-18 17:00:12 -0700

    Initial commit

---
+++ Migrap.ApplicationInsights/TelemetryClientExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.ApplicationInsights;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Migrap.ApplicationInsights {
+    public delegate Func<string> TrackEventName(TelemetryClient client = null);
+
+    public static class TelemetryClientExtensions {
+        public static void TrackEvent(this TelemetryClient client, Func<TelemetryClient, Func<string>> name, IDictionary<string, string> properties = null, IDictionary<string, double> metrics = null) {
+            client.TrackEvent(name(client)(), properties, metrics);
+        }
+
+        public static void TrackEvent(this TelemetryClient client, Func<TelemetryClient, string> name, IDictionary<string, string> properties = null, IDictionary<string, double> metrics = null) {
+            client.TrackEvent(name(client), properties, metrics);
+        }
+
+        public static void TrackEvent(this TelemetryClient client, string name, object properties = null, IDictionary<string, double> metrics = null) {
+            client.TrackEvent(name, properties.AsDictionary(), metrics);
+        }
+
+        public static void TrackEvent(this TelemetryClient client, Func<TelemetryClient, Func<string>> name, object properties = null, IDictionary<string, double> metrics = null) {
+            client.TrackEvent(name(client)(), properties.AsDictionary(), metrics);
+        }
+
+        public static void TrackEvent(this TelemetryClient client, Func<TelemetryClient, string> name, object properties = null, IDictionary<string, double> metrics = null) {
+            client.TrackEvent(name(client), properties.AsDictionary(), metrics);
+        }
+
+        private static IDictionary<string, string> AsDictionary(this object value) {
+            return value.GetType().GetProperties().ToDictionary(x => x.Name, x => x.GetValue(value).ToString());
+        }
+    }
+}
